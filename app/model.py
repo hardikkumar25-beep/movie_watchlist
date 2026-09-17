087d22974d42bb3e66b5aed968a9ced1023b2915
hardikkumar25-beep <hardikkumar.work25@gmail.com>
2026-09-18 00:30:59 +0530

    Added watchlist and movies table in db

--- app/model.py
+++ app/model.py
@@ -1,6 +1,7 @@
 from app.database import Base
 from sqlalchemy.orm import mapped_column,Mapped
-from sqlalchemy import String
+from sqlalchemy.sql import func
+from sqlalchemy import String, Column, Integer, Float,ForeignKey,DateTime
 
 class User(Base):
     __tablename__="users"
@@ -8,3 +9,22 @@ class User(Base):
     id: Mapped[int]=mapped_column(primary_key=True)
     username:Mapped[str]=mapped_column(String(50))
     email:Mapped[str]=mapped_column(String(100),unique=True)
+
+class Movies(Base):
+    __tablename__="movies"
+
+    id=Column(Integer, primary_key=True)
+    tmdb_id=Column(Integer,primary_key=True,nullable=False)
+    title=Column(String,nullable=False)
+    description=Column(String)
+    release_date=Column(Integer)
+    rating=Column(Float)
+    poster_url=Column(String)
+
+class Watchlist(Base):
+    __tablename__="watchlist"
+
+    id = Column(Integer, primary_key=True)
+    user_id=Column(Integer,ForeignKey("users.id"),nullable=False)
+    movie_id=Column(Integer,ForeignKey("movies.id"),nullable=False)
+    added_at=Column(DateTime,server_default=func.now())
